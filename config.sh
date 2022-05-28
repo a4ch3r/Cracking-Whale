ef10ca32f008ba575e873aaf535f6aa6c6f0585a
81rdy <101480715+81rdy@users.noreply.github.com>
2022-05-28 21:19:22 +0200

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+distro_name=$(egrep '^NAME="' /etc/os-release)
+echo "$distro_name"
+
+if [ "$distro_name" = 'NAME="Ubuntu"' ];then
+    sudo apt update && sudo apt upgrade -y;
+    reboot;
+    sudo apt update -y;
+    sudo sh -c "echo 'deb https://http.kali.org/kali kali-rolling main non-free contrib' > /etc/apt/sources.list.d/kali.list";
+    sudo apt install gnupg;
+    wget 'https://archive.kali.org/archive-key.asc'
+    sudo apt-key add archive-key.asc;
+    sudo apt update;
+    sudo sh -c "echo 'Package: *'>/etc/apt/preferences.d/kali.pref; echo 'Pin: release a=kali-rolling'>>/etc/apt/preferences.d/kali.pref; echo 'Pin-Priority: 50'>>/etc/apt/preferences.d/kali.pref"
+    sudo apt update;
+    sudo apt install aptitude -y;
+    sudo aptitude install -t kali-rolling P A K I E T Y
+fi
+#if($distro_name == 'NAME="Ubuntu"')S
+
+#if($distro_name == 'NAME="Ubuntu"')
+
+#if($distro_name == 'NAME="Ubuntu"')
+
+#if($distro_name == 'NAME="Ubuntu"')
+~                                                                                                                                                                                                          
+~                                           
